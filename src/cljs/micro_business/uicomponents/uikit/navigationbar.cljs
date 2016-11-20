27868a4c2f1501f61aaca63e5f71d7a20b94a9ba
Morteza ALizadeh <morteza.alizadeh@gmail.com>
2016-11-20 23:48:11 +1300

    Fixed variable name in navigation bar

--- src/cljs/micro_business/uicomponents/uikit/navigationbar.cljs
+++ src/cljs/micro_business/uicomponents/uikit/navigationbar.cljs
@@ -9,18 +9,18 @@
     (dom/ul navbarNavStyle
             (map navigationitem/navItem navigationItems))))
 
-(defn getNavbar [{:keys [brand rightToLeftAlignment navigationItems]}]
+(defn- getNavbar [{:keys [brand rightToLeftAlignment navigationItems]}]
   (let [navbarStyle #js {:className "uk-navbar uk-margin-large-bottom"}
         navbarFlipStyle #js {:className "uk-navbar-flip"}
         navbarBrandStyle #js {:className "uk-navbar-brand uk-hidden-small" :href "#"}
-        getNavbarOffCanvasStyle #js {:className "uk-navbar-toggle uk-visible-small" :href "#offcanvas" :data-uk-offcanvas ""}
+        navbarOffCanvasStyle #js {:className "uk-navbar-toggle uk-visible-small" :href "#offcanvas" :data-uk-offcanvas ""}
         navbarBrandForSmallDeviceStyle #js {:className "uk-navbar-brand uk-navbar-center uk-visible-small" :href "#"}]
     (apply dom/nav navbarStyle [(dom/a navbarBrandStyle brand)
                                 (if rightToLeftAlignment
                                   (getNavigationItems navigationItems))
                                 (dom/div navbarFlipStyle
                                          (getNavigationItems navigationItems))
-                                (dom/a getNavbarOffCanvasStyle)
+                                (dom/a navbarOffCanvasStyle)
                                 (dom/a navbarBrandForSmallDeviceStyle brand)])))
 
 (defui Navbar
