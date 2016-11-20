1ee04b4dc519eaa7226b845266911404f6930437
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2016-11-20 16:47:25 +1300

    Added UIKit navigation bar and item

--- src/cljs/micro_business/uicomponents.cljs
+++ src/cljs/micro_business/uicomponents.cljs
@@ -1,4 +1,1 @@
-(ns micro-business.uicomponents
-  (:require [goog.dom :as gdom]
-            [om.next :as om :refer-macros [defui]]
-            [om.dom :as dom]))
+(ns micro-business.uicomponents)
