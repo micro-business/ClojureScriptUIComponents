37286bdc1479ff60695f3cc69dac9fc90097269e
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2016-11-28 23:28:41 +1300

    Removed ids from navigation tiem

--- src/cljs/micro_business/state.cljs
+++ src/cljs/micro_business/state.cljs
@@ -3,10 +3,10 @@
 (def applicationGlobalState
   {:navigationBar {:brand "Brand"
                    :rightToLeftAlignment false
-                   :navigationItems [{:id "item1" :caption "Item1" :onClickedDispatchKey 'item1/clicked}
-                                     {:id "item2" :caption "Item2" :onClickedDispatchKey 'item2/clicked}
-                                     {:id "item3" :caption "Item3" :href "#item3"}
-                                     {:id "item4" :caption "Item4" :href "#item4"}
-                                     {:id "item5" :caption "Item5" :onClickedDispatchKey 'item5/clicked}
-                                     {:id "item6" :caption "Item6" :href "#item6"}
-                                     {:id "item7" :caption "Item7" :href "#item7"}]}})
+                   :navigationItems [{:caption "Item1" :onClickedDispatchKey 'item1/clicked}
+                                     {:caption "Item2" :onClickedDispatchKey 'item2/clicked}
+                                     {:caption "Item3" :href "#item3"}
+                                     {:caption "Item4" :href "#item4"}
+                                     {:caption "Item5" :onClickedDispatchKey 'item5/clicked}
+                                     {:caption "Item6" :href "#item6"}
+                                     {:caption "Item7" :href "#item7"}]}})
